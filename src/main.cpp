ecbaf4cb4e015b46195d87043a9dea9ab70adfb3
ftoledo-c <f.toledo@ufl.edu>
2026-03-27 23:04:16 +0000

    Feat: Implement main program loop and switch logic to construct UI that works with Data Processing and Heap Sort (Issue #7)

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
+#include "data_processing.h"
+#include "UI.h"
+#include "comparators.h"
+#include "heap_sort.h"
 #include <iostream>
 #include <vector>
-#include "data_processing.h"
 
-int main() {
+
+int main() {    
     std::cout << "Delayradar24: Processing Flights..." << std::endl;
-
+    
     // Initiates data processing
     std::vector<AirportData> data = loadAndAggregateData("flight_data_2024.csv");
 
@@ -14,14 +18,49 @@ int main() {
         return 1;
     }
 
-    // Total num of airports processed
-    std::cout << "Processed " << data.size() << " airports." << std::endl;
+    // Main program loop
+    while (true) {
+        int choice = getMenuChoice();
 
-    // Print the airport at  index 0 in the vector for testing
-    std::cout << "Sample: " << data[0].airportCode << " average delay is " << data[0].averageDelay << " mins." << std::endl;
+        // Determine sorting choice based on user input
+        switch (choice) {
+            case 1: // Highest First (VECTOR is sorted descending)
+                heapSort(data, DescendingDelay{});
+                // quicksort
+                break;
 
-    // Print the airport at the given index in the vector for testing
-    std::cout << "Sample: " << data[320].airportCode << " average delay is " << data[320].averageDelay << " mins." << std::endl;
+            case 2: // Lowest First (VECTOR is sorted ascending)
+                heapSort(data, AscendingDelay{});
+                //quicksort
+                break;
 
+            case 3: // A First (VECTOR is sorted ascending lexicographically)
+                heapSort(data, AscendingLexi{});
+                // quicksort
+                break;
+
+            case 4: // Z First (VECTOR is sorted descending lexicographically)
+                heapSort(data, DescendingLexi{});
+                // quicksort
+                break;
+
+            case 5: // Exit
+                std::cout << "Have a good flight!!" << std::endl;
+                return 0;
+        }
+
+        // Prompt user for how long their ranked list should be
+        std::cout << "How many Airports do you want to view in the ranking? (1-334)" << std::endl;
+        
+        // Retrieves valid user input and prints the sorted data accordingly
+        size_t num = getValidListSize();
+        printRanking(data, num);
+        
+        // Determine if program should exit or loop back to menu
+        std::cout << "Would you like to go again? (Y/N)" << std::endl;
+        if (!shouldContinue()) {
+            return 0;
+        }
+    }
     return 0;
 }
