924c7232f60920a2726d81236c74797ca95b80a3
ftoledo-c <f.toledo@ufl.edu>
2026-03-27 22:32:16 +0000

    Fix: Flip comparison logic in Lexi comparators to correct sort order (Issue #5)

--- src/comparators.h
+++ src/comparators.h
@@ -18,13 +18,13 @@ struct DescendingDelay {
 // Comparator to sort airports by their 3-letter code in alphabetical order (A-Z)
 struct AscendingLexi {
     bool operator()(const AirportData& a, const AirportData& b) const {
-        return a.airportCode < b.airportCode;
+        return a.airportCode > b.airportCode;
     }
 };
 
 // Comparator to sort airports by their 3-letter code in reverse alphabetical order (Z-A)
 struct DescendingLexi {
     bool operator()(const AirportData& a, const AirportData& b) const {
-        return a.airportCode > b.airportCode;
+        return a.airportCode < b.airportCode;
     }
 };
